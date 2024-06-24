0e250cfd710bca038a362010fa4c455357203b22
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-06-24 17:55:54 -0600

    exceptions

---
+++ hello.py
@@ -0,0 +1,14 @@
+def divide():
+
+    try:
+        op1=(float(input("digit the fist numeric value: ")))
+        op2=(float(input("digit the second numeric value: ")))
+        print("the division is: " + str(op1/op2))
+    except ValueError:
+        print("the value is not acceptable")
+    except ZeroDivisionError:
+        print("zero division not allow")
+
+    print("operation finished")
+
+divide()
