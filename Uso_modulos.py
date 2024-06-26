f86532a6ac9a7e4fe720a76aac341b6aab0e862f
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-06-26 09:37:26 -0600

    paquetes y ejemplo

---
+++ Uso_modulos.py
@@ -0,0 +1,5 @@
+from calculos.calculos_generales import *
+
+dividir(10,2)
+
+potencia(2,2)
---
+++ calculos/calculos_generales.py
@@ -0,0 +1,22 @@
+def sumar(op1,op2):
+    print("El resultado de la suma es: ", op1+op2)
+
+
+def restar(op1,op2):
+    print("El resultado de la resta es: ", op1-op2)
+
+
+def multiplicar(op1,op2):
+    print("El resultado de la miltiplicación es: ", op1*op2)
+
+
+def dividir(op1,op2):
+    print("El resultado de la división es: ", op1/op2)
+
+
+def potencia(op1,op2):
+    print("El resultado de la potencia es: ", op1**op2)
+
+
+def redondear(op1):
+    print("El resultado del redondeo es: ", round(op1))
